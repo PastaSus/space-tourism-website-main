c050d270da6071ccb10eda0c0ad9952c1d4c681b
Aaron Cerezo <aarongtxd@gmail.com>
2025-08-23 22:50:21 +0800

    update markup accessibility,focus state for links, readMe, add focusTrap for accessibility in modals

--- space-tourism-website/src/main.js
+++ space-tourism-website/src/main.js
@@ -2,38 +2,99 @@ const openNavBtn = document.querySelector(".nav__open");
 const closeNavBtn = document.querySelector(".nav__close");
 const nav = document.querySelector(".nav");
 
-// hides our burger and header-nav in larger screens(tablets above)
-document.addEventListener("DOMContentLoaded", updateHeaderAria);
-window.addEventListener("resize", updateHeaderAria);
+openNavBtn.addEventListener("click", animateOpenNav);
+closeNavBtn.addEventListener("click", animateCloseNav);
 
-openNavBtn.addEventListener("click", openNav);
-closeNavBtn.addEventListener("click", closeNav);
+// side nav UI on tablets above
+document.addEventListener("DOMContentLoaded", changeNavHeader);
+window.addEventListener("resize", changeNavHeader);
 
-// For header nav
-function updateHeaderAria() {
-  const isDesktop = window.innerWidth >= 768;
+function changeNavHeader() {
+  const isTabletAbove = window.innerWidth >= 768;
 
-  nav.setAttribute("aria-hidden", isDesktop ? "false" : "true");
+  nav.hidden = !isTabletAbove;
 }
 
-function openNav() {
-  nav.classList.remove("translate-x-full", "opacity-0");
-  nav.classList.add("translate-x-0", "opacity-100");
+let removeTrapFocus; // store cleanup function
 
-  openNavBtn.setAttribute("aria-expanded", "true");
-  closeNavBtn.setAttribute("aria-expanded", "true");
-  // For header nav
-  nav.setAttribute("aria-hidden", "false");
+// utility function - reusable (hide from ATs)
+function animateOpenNav() {
+  const isClose = nav.classList.contains("translate-x-full");
+
+  if (isClose) {
+    // makes it appear in the DOM first
+    nav.hidden = false;
+
+    // what follows is we request some frames to animate the slide
+    requestAnimationFrame(() => {
+      nav.classList.remove("translate-x-full", "opacity-0");
+      nav.classList.add("translate-x-0", "opacity-100");
+    });
+
+    openNavBtn.setAttribute("aria-expanded", "true");
+
+    // activate focus trap - still runs the function call
+    removeTrapFocus = trapFocus(nav);
+
+    closeNavBtn.focus();
+  }
+}
+function animateCloseNav() {
+  const isOpen = nav.classList.contains("translate-x-0");
+
+  if (isOpen) {
+    requestAnimationFrame(() => {
+      nav.classList.add("translate-x-full", "opacity-0");
+    });
+
+    nav.addEventListener(
+      "transitionend",
+      () => {
+        if (nav.classList.contains("translate-x-full")) nav.hidden = true;
+      },
+      { once: true },
+    );
+
+    openNavBtn.setAttribute("aria-expanded", "false");
+    openNavBtn.focus();
+
+    // cleanup trap focus eventListener attached when closed
+    if (removeTrapFocus) removeTrapFocus();
+  }
 }
 
-function closeNav() {
-  nav.classList.add("translate-x-full", "opacity-0");
+// TODO: for accessibility
+function trapFocus(container) {
+  const navFocusables = container.querySelectorAll(".nav__close, .nav__link");
 
-  openNavBtn.setAttribute("aria-expanded", "false");
-  closeNavBtn.setAttribute("aria-expanded", "false");
+  const firstEl = navFocusables[0];
+  const lastEl = navFocusables[navFocusables.length - 1];
 
-  // For header nav
-  nav.setAttribute("aria-hidden", "true");
+  function handleNavTrap(e) {
+    const isMobile = window.innerWidth < 768;
 
-  openNavBtn.focus();
+    if (e.key !== "Tab") return;
+
+    // check width at the moment of keydown
+    if (!isMobile) return;
+
+    // e.shiftKey - shift + tab condition
+    if (e.shiftKey) {
+      if (document.activeElement === firstEl) {
+        e.preventDefault();
+        lastEl.focus();
+      }
+    } else {
+      if (document.activeElement === lastEl) {
+        e.preventDefault();
+        firstEl.focus();
+      }
+    }
+  }
+
+  container.addEventListener("keydown", handleNavTrap);
+
+  return () => container.removeEventListener("keydown", handleNavTrap);
 }
+
+// trapFocus(nav);
